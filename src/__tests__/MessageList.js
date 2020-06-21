b263bf1078b7d26ef3eb7b25a2ecef491c70cc77
Atheane <eroyant@gmail.com>
2020-06-21 13:21:42 +0200

    render with @testing-library/react instead of shallow rendering with enzyme

--- src/__tests__/MessageList.js
+++ src/__tests__/MessageList.js
@@ -1,6 +1,5 @@
 import React from "react"
-// import { render } from '@testing-library/react'
-import { shallow } from "enzyme"
+import { render } from "@testing-library/react"
 
 import axios from "axios"
 
@@ -12,14 +11,13 @@ describe("MessageList component", () => {
   describe("when rendered", () => {
     it("should fetch a list of messages", () => {
       const getSpy = jest.spyOn(axios, "get")
-      const messageListInstance = shallow(
+      render(
         <MessageList
           messages={[
             { createdAt: "2020-06-21T10:18:22.045Z", message: "plop" },
           ]}
         />
       )
-      console.log(messageListInstance)
       expect(getSpy).toBeCalled()
     })
   })
