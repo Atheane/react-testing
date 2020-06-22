b1474ee6be9e388ede3cd0adfdc4703f937e4ced
Atheane <eroyant@gmail.com>
2020-06-22 08:54:05 +0200

    text area UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react"
 import "antd/dist/antd.css"
+import SimpleBar from "simplebar-react"
 import Header from "./components/Header"
 import MessageList from "./containers/MessageList"
+import TextArea from "./containers/TextArea"
+import "simplebar/dist/simplebar.min.css"
+
 import "./assets/scss/App.scss"
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <MessageList />
+      <SimpleBar>
+        <MessageList />
+        <TextArea />
+      </SimpleBar>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,11 @@ import { PageHeader } from "antd"
 import logo from "../assets/images/BattlestartGallactica.jpg"
 import "../assets/scss/Header.scss"
 
-const Header = () => {
-  return (
-    <header className="header">
-      <img src={logo} className="logo" alt="logo" />
-      <PageHeader title="Battlestar Galactitchache" />
-    </header>
-  )
-}
+const Header = () => (
+  <header className="header">
+    <img src={logo} className="logo" alt="logo" />
+    <PageHeader title="Battlestar Galactitchache" />
+  </header>
+)
 
 export default Header
